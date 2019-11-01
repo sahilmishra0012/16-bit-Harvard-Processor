03aa5228f93ca10b127d99341062bbaf49f21e82
Sahil Mishra <sahilmishra0012@outlook.com>
2019-11-01 09:56:38 +0530

    Or Gate Module Completed

---
+++ or.v
@@ -0,0 +1,3 @@
+module or_gate(input [15:0] A,input [15:0] B, output [15:0] OR);
+    assign OR = A|B;
+endmodule
--- testbench.v
+++ testbench.v
@@ -1,14 +1,15 @@
-`include "not.v"
+`include "or.v"
 module tb;
         reg [15:0] a;
+        reg [15:0] b;
         wire [15:0] out;
-        reverse nott(a,out);
+        or_gate nott(a,b,out);
         initial 
             begin
-                a = 16'b0;#100;
-                a = 16'b1110;#200;
-                a = 16'b0000011;#300;   
+                a = 16'b0;b = 16'b01;#100;
+                a = 16'b1110;b = 16'b010101;#200;
+                a = 16'b0000011;b = 16'b11011101;#300;   
             end
         initial
-        $monitor("A= %b :: Output= %b",a,out);
+        $monitor("A= %b :: B= %b :: Output= %b",a,b,out);
 endmodule
