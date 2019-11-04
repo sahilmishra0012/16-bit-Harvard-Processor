f43fda1113dc3637b4dfa7305a0b8c1bb5770d02
Sahil Mishra <sahilmishra0012@outlook.com>
2019-11-04 12:03:17 +0530

    Redundant files deleted

--- ALU/Left Shift/left_shift.v
+++
@@ -1,3 +0,0 @@
-module left_gate(input [15:0] A, output [15:0] shifted);
-    assign shifted = A * 2;
-endmodule
--- ALU/Right Shift/right_shift.v
+++
@@ -1,3 +0,0 @@
-module right_gate(input [15:0] A, output [15:0] shifted);
-    assign shifted = A / 2;
-endmodule
