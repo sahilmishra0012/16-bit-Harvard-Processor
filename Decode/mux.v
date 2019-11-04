055ab17134e428aa0547d7c18be05c6e3f1d561c
Sahil Mishra <sahilmishra0012@outlook.com>
2019-11-04 12:12:05 +0530

    New Changes

---
+++ Decode/mux.v
@@ -0,0 +1,17 @@
+module mux21(select,in,in1,out);
+
+input  select;
+input  in,in1;
+output out;
+reg  out;
+wire  select;
+
+always@(select or in or in1)
+begin
+if(select == 0)
+out=in1;
+if(select == 1)
+out=in;
+end
+
+endmodule
