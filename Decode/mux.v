593884c6137d638311b1e022531d4e1bcb4caf3a
Sahil Mishra <sahilmishra0012@outlook.com>
2019-11-04 13:03:00 +0530

    New Changes

--- Decode/mux.v
+++ Decode/mux.v
@@ -1,11 +1,5 @@
-module mux21(select,in,in1,out);
-
-input  select;
-input  in0,in1,in2,in3,in4,in5,in6,in7,in8;
-input  in9,in10,in11,in12,in13,in14,in15,in16,in17;
-output out;
+module mux21(input select, output out);
 reg  out;
-wire  select;
 
 always@(select or in0 or in1 or in2 or in3 or in4 or in5 or in6 or in7 or in8 or in9 or in10 or in11 or in12 or in13 or in14 or in15 or in16 or in17)
 begin
