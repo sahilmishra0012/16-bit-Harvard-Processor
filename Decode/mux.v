b1519a64981b0d4a8873433c4a53fe1cee7d3330
Sahil Mishra <sahilmishra0012@outlook.com>
2019-11-05 23:13:58 +0530

    New Changes

--- Decode/mux.v
+++ Decode/mux.v
@@ -1,71 +1,69 @@
-`include"/home/samthekiller/Desktop/Harvard Architecture/My_Code/ALU/barrel_left/barrel16.v"
-`include"/home/samthekiller/Desktop/Harvard Architecture/My_Code/ALU/barrel_right/barrel16r.v"
+`include"/home/samthekiller/Desktop/Harvard Architecture/My_Code/ALU/barrel_left/barrel16.vl"
+`include"/home/samthekiller/Desktop/Harvard Architecture/My_Code/ALU/barrel_right/barrel16r.vl"
 `include"/home/samthekiller/Desktop/Harvard Architecture/My_Code/ALU/NAND Gate/nand_custom.v"
 `include"/home/samthekiller/Desktop/Harvard Architecture/My_Code/ALU/NOR Gate/nor_custom.v"
 `include"/home/samthekiller/Desktop/Harvard Architecture/My_Code/ALU/NOT Gate/not_custom.v"
 `include"/home/samthekiller/Desktop/Harvard Architecture/My_Code/ALU/OR Gate/or_custom.v"
 `include"/home/samthekiller/Desktop/Harvard Architecture/My_Code/ALU/XNOR Gate/xnor_custom.v"
 `include"/home/samthekiller/Desktop/Harvard Architecture/My_Code/ALU/XOR Gate/xor_custom.v"
-`include"/home/samthekiller/Desktop/Harvard Architecture/My_Code/ALU/Recursive Adder/Padder.vl"
-`include"/home/samthekiller/Desktop/Harvard Architecture/My_Code/ALU/Recursive Adder/Padder_Subtractor.vl"
-`include "/home/samthekiller/Desktop/Harvard Architecture/My_Code/ALU/Carry Save Array Multiplier/multiplier16bit.v"
 `include "/home/samthekiller/Desktop/Harvard Architecture/My_Code/ALU/negative/negate_custom.v"
 
 
-module mux21(input [31:0]code, output out);
+module muxfinal(input [31:0]code, output out);
 reg  out;
-wire select,Rdst2,Rdst1,Rsrc2,Rsrc1,RsrcAdd,RdstAdd;
+reg  [5:0] Rdst2,Rdst1,Rsrc2,Rsrc1;
+reg [8:0] RsrcAdd,RdstAdd;
 assign select=code[31:26];
 reg c;
 always@(select)
     case(select)
         000000: begin
-            assign Rdst2=code[25:21];
-            assign Rsrc2=code[15:0];
+            Rdst2=code[25:21];
+            Rsrc2=code[15:0];
         end
 
 
         000001: begin
-            assign Rdst2=code[25:21];
-            assign Rsrc2=code[4:0];
+            Rdst2=code[25:21];
+            Rsrc2=code[4:0];
         end
         000010: begin
-            assign Rdst2=code[25:21];
-            assign RsrcAdd=code[7:0];
+            Rdst2=code[25:21];
+            RsrcAdd=code[7:0];
         end
         000011: begin
         
-            assign RdstAdd=code[25:18];
-            assign Rsrc2=code[4:0];
+            RdstAdd=code[25:18];
+            Rsrc2=code[4:0];
         end
         000100: begin
-            assign Rdst2=code[25:21];
-            assign Rdst1=code[20:16];
-            assign Rsrc2=code[9:5];
-            assign Rsrc1=code[4:0];
+            Rdst2=code[25:21];
+            Rdst1=code[20:16];
+            Rsrc2=code[9:5];
+            Rsrc1=code[4:0];
             c=2'b00;
             // adder_rd mod100(Rsrc2,Rsrc1,c,Rdst1);
         end
         000101: begin
-            assign Rdst2=code[25:21];
-            assign Rdst1=code[20:16];
-            assign Rsrc2=code[9:5];
-            assign Rsrc1=code[4:0];
+            Rdst2=code[25:21];
+            Rdst1=code[20:16];
+            Rsrc2=code[9:5];
+            Rsrc1=code[4:0];
             c=2'b00;
             // sub_rd mod101(Rsrc2,Rsrc1,c,Rdst1);
         end
         000110: begin
-            assign Rdst2=code[25:21];
-            assign Rdst1=code[20:16];
-            assign Rsrc2=code[9:5];
-            assign Rsrc1=code[4:0];
+            Rdst2=code[25:21];
+            Rdst1=code[20:16];
+            Rsrc2=code[9:5];
+            Rsrc1=code[4:0];
             // negative mod110(Rsrc1,Rdst1);
         end
         000111: begin
-            assign Rdst2=code[25:21];
-            assign Rdst1=code[20:16];
-            assign Rsrc2=code[9:5];
-            assign Rsrc1=code[4:0];
+            Rdst2=code[25:21];
+            Rdst1=code[20:16];
+            Rsrc2=code[9:5];
+            Rsrc1=code[4:0];
             // multiplier mod111(Rsrc2,Rsrc1,Rdst1);
         end
         001000: begin
@@ -130,6 +128,6 @@ always@(select)
             Rsrc1=code[4:0];
             // barrel_right mod10000(Rsrc2,Rsrc1,Rdst1);
         end
-
+    endcase
 
 endmodule
