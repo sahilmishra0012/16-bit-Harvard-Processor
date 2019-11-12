e4370b1ec4fdf7e3abe0c333ea6e5d1e31a7fd82
Sahil Mishra <sahilmishra0012@outlook.com>
2019-11-12 18:08:08 +0530

    New Changes

--- Register File/reg.v
+++ Register File/reg.v
@@ -1,31 +1,21 @@
 module register_file  
 (  
-    input                    clk,  
-    input                    rst,  
-    // write port  
-    input                    reg_write_en,  
-    input          [2:0]     reg_write_dest,  
-    input          [15:0]     reg_write_data,  
-    //read port 1  
-    input          [2:0]     reg_read_addr_1,  
-    output          [15:0]     reg_read_data_1,  
-    //read port 2  
-    input          [2:0]     reg_read_addr_2,  
-    output          [15:0]     reg_read_data_2  
+    input clk,  
+    input rst,  
+    input reg_write_en,  
+    input [2:0] reg_write_dest,  
+    input [15:0] reg_write_data,  
+    input [2:0] reg_read_addr_1,  
+    output [15:0] reg_read_data_1,  
+    input [2:0] reg_read_addr_2,  
+    output [15:0] reg_read_data_2  
 );  
-    reg     [15:0]     reg_array [7:0];  
-    // write port  
-    //reg [2:0] i;  
+    integer i;
+    reg [15:0] reg_array [31:0];    
     always @ (posedge clk or posedge rst) begin  
         if(rst) begin  
-            reg_array[0] <= 16'b0;  
-            reg_array[1] <= 16'b0;  
-            reg_array[2] <= 16'b0;  
-            reg_array[3] <= 16'b0;  
-            reg_array[4] <= 16'b0;  
-            reg_array[5] <= 16'b0;  
-            reg_array[6] <= 16'b0;  
-            reg_array[7] <= 16'b0;       
+            for(i=0;i<=31;i=i+1)  
+                reg_array[i] = 16'b0;      
         end  
         else begin  
             if(reg_write_en) begin  
