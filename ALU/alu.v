769f2369d3e42e5ba9a11107a5d2fb3b7051f37d
Sahil Mishra <sahilmishra0012@outlook.com>
2019-11-12 11:59:28 +0530

    New Changes

--- ALU/alu.v
+++ ALU/alu.v
@@ -14,23 +14,24 @@
 `include "/home/samthekiller/Desktop/Harvard Architecture/My_Code/ALU/NOT Gate/not_custom.v"
 `include "/home/samthekiller/Desktop/Harvard Architecture/My_Code/ALU/Shift/right_shift.v"
 `include "/home/samthekiller/Desktop/Harvard Architecture/My_Code/ALU/Shift/left_shift.v"
-module alumodule(
-input [31:0] code,
-output [15:0] sum,
-output [15:0] diff,
-output [15:0] negate,
-output [31:0] multiplied,
-output [15:0] divi,
-output [15:0] or_gat,
-output [15:0] xor_gat,
-output [15:0] nand_gat,
-output [15:0] nor_gat,
-output [15:0] xnor_gat,
-output [15:0] not_gat,
-output [15:0] left_sft,
-output [15:0] right_sft,
-output wire [5:0] select
-);
+module alumodule(input [31:0] code);
+reg  [31:0] out;
+wire [15:0] out1;
+wire [15:0] out2;
+wire [15:0] sum;
+wire [15:0] diff;
+wire [15:0] negate;
+wire [31:0] multiplied;
+wire [15:0] divi;
+wire [15:0] or_gat;
+wire [15:0] xor_gat;
+wire [15:0] nand_gat;
+wire [15:0] nor_gat;
+wire [15:0] xnor_gat;
+wire [15:0] not_gat;
+wire [15:0] left_sft;
+wire [15:0] right_sft;
+wire [5:0] select;
 wire [5:0] Rdst2;
 wire [5:0] Rdst1;
 wire [5:0] Rsrc2;
@@ -47,29 +48,75 @@ memory mem1(Rsrc2,b,1'b1);
 memory mem2(Rsrc1,a,1'b1);
 
 create_tb mod00(b,a,2'b0,sum);
-
 create_tb mod01(b,~a,2'b0,diff);
-
 negative neg(a,negate);
-
 multiplier mult(a,b,multiplied);
-
 Divide div(b,a,divi);
-
 or_gate or1(b,a,or_gat);
-
 xor_gate xor1(b,a,xor_gat);
-
 nand_gate nand1(b,a,nand_gat);
-
 nor_gate nor1(b,a,nor_gat);
-
 xnor_gate xnor1(b,a,xnor_gat);
-
 not_gate not1(a,not_gat);
-
 left lft1(b,a,left_sft);
-
 right rght1(b,a,right_sft);
+always@(*)
+    case(select)
+        // 6'b000000: begin//MOV
+
+        // end
+        // 000001: begin//MOV
+
+        // end
+        // 000010: begin//LOAD
+
+        // end
+        // 000011: begin//STORE
+
+        // end
+        000100: begin//ADD
+        out[15:0]=sum;
+        end
+        000101: begin//SUB
+        out[15:0]=diff;
+        end
+        000110: begin//NEG
+        out[15:0]=negate;
+        end
+        000111: begin//MUL
+        out[31:0]=multiplied;
+        end
+        001000: begin//DIV
+        out[15:0]=divi;
+        end
+        001001: begin//OR
+        out[15:0]=or_gat;
+        end
+        001010: begin//XOR
+        out[15:0]=xor_gat;
+        end
+        001011: begin//NAND
+        out[15:0]=nand_gat;
+        end
+        001100: begin//NOR
+        out[15:0]=nor_gat;
+        end
+        001101: begin//XNOR
+        out[15:0]=xnor_gat;
+        end
+        001110: begin//NOT
+        out[15:0]=not_gat;
+        end
+        001111: begin//LLSH
+        out[15:0]=left_sft;
+        end
+        010000: begin//LRSH
+        out[15:0]=right_sft;
+        end
+    endcase
+    assign out1[15:0]=out[15:0];
+    assign out2[15:0]=out[31:15];
+    memory mem21(Rdst1,out1,1'b0);
+    memory mem31(Rdst2,out2,1'b0);
 
 endmodule
