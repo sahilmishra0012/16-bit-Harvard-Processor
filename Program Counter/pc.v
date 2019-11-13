2eefa382c7083845bd7761a0192b0c32be327fff
Sahil Mishra <sahilmishra0012@outlook.com>
2019-11-13 20:42:49 +0530

    New Changes

--- Program Counter/pc.v
+++ Program Counter/pc.v
@@ -1,13 +1,14 @@
-module PC(clk, load, reset, D, count);
-    input clk, load, reset;
-	 input [15:0] D;
-    output reg [15:0] count;
+module up_counter(input clk, reset, output[5:0] counter
+    );
+reg [5:0] counter_up;
 
-    always @(posedge clk or negedge reset)
-			if (~reset)
-				count <= 16'b0;
-			else if (load) 
-                count <= D;  
-            else
-				count <= count + 1'b1;
+// up counter
+always @(posedge clk or posedge reset)
+begin
+if(reset)
+	counter_up <= 6'd0;
+else
+	counter_up <= counter_up + 6'd1;
+end 
+assign counter = counter_up;
 endmodule
--- test_bench.v
+++ test_bench.v
@@ -1,32 +1,22 @@
-    `timescale 1ns / 1ps
-    //fpga4student.com: FPGA projects, Verilog projects, VHDL projects
-    // Verilog project: Verilog code for 16-bit MIPS Processor
-    // Testbench Verilog code for 16 bit single cycle MIPS CPU  
-    module tb_mips16;  
-        // Inputs  
-        reg clk;  
-        reg reset;  
-        // Outputs  
-        wire [15:0] pc_out;  
-        wire [15:0] alu_result;//,reg3,reg4;  
-        // Instantiate the Unit Under Test (UUT)  
-        mips_16 uut (  
-            .clk(clk),   
-            .reset(reset),   
-            .pc_out(pc_out),   
-            .alu_result(alu_result)  
-        );  
-        initial begin  
-            clk = 0;  
-            forever #10 clk = ~clk;  
-        end  
-        initial begin  
-            // Initialize Inputs  
-            //$monitor ("register 3=%d, register 4=%d", reg3,reg4);  
-            reset = 1;  
-            // Wait 100 ns for global reset to finish  
-            #100;  
-        reset = 0;  
-            // Add stimulus here  
-        end  
-    endmodule  
+`include "/home/samthekiller/Desktop/Harvard Architecture/My_Code/Program Counter/pc.v"
+module upcounter_testbench();
+reg clk, reset;
+wire [5:0] counter;
+
+up_counter dut(clk, reset, counter);
+initial 
+begin 
+clk=0;
+forever #5 clk=~clk;
+end
+initial
+begin
+reset=1;
+#20;
+reset=0;
+end
+initial
+#1930 $finish;
+initial
+$monitor("Out => %d",counter);
+endmodule 
