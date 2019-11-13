3a61ddd6cb1027dae792eb83052743db353e90e8
Sahil Mishra <sahilmishra0012@outlook.com>
2019-11-13 21:11:32 +0530

    New Changes

--- test_bench.v
+++ test_bench.v
@@ -1,9 +1,14 @@
 `include "/home/samthekiller/Desktop/Harvard Architecture/My_Code/Program Counter/pc.v"
+`include "/home/samthekiller/Desktop/Harvard Architecture/My_Code/Instruction_Set/ins.v"
 module upcounter_testbench();
 reg clk, reset;
 wire [5:0] counter;
+wire [5:0] ins_padder;
+wire [31:0] Instruction_out;
 
 up_counter dut(clk, reset, counter);
+assign ins_padder=counter;
+Instruction_Mem mem1(Instruction_out, ins_padder);
 initial 
 begin 
 clk=0;
@@ -16,7 +21,7 @@ reset=1;
 reset=0;
 end
 initial
-#1930 $finish;
+#650 $finish;
 initial
-$monitor("Out => %d",counter);
+$monitor("Instruction Number => %d || Instruction => %b",counter,Instruction_out);
 endmodule 
